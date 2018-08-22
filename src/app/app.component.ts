63f820ce0aed68961646cab6b4f6a5cb445d0c59
Oleksandr Vyshnevskyi <mrrgoodcat@gmail.com>
2018-08-22 17:44:54 +0300

    all done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,8 @@ export class AppComponent {
   
   items: FirebaseListObservable<any[]>;
   msg: string = '';
+  editMsg: boolean = false;
+  editId: number;
 
   constructor(public af: AngularFireDatabase) {
     this.items = af.list('/messages');
@@ -18,5 +20,14 @@ export class AppComponent {
   send(chatMsg: string) {
     this.items.push({ message: chatMsg });
     this.msg = '';
+  }
+
+  delete(key:string){
+    this.items.remove(key);
+  }
+
+  edit(key: string, message: string){
+    this.items.update(key, {message: message});
+    this.editMsg = false;
   }
 }
